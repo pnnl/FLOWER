50e63387f21bad7edf59d246e905504237c5e83f
Jason Mount <Jason.Mount@pnnl.gov>
2017-09-26 23:48:07 -0700

    Added Visual Studio 2017 build configuration Added Boost Unit Test Adapter projects for t_Flow and t_Data
    TODO: add the rest of the tests
    TODO: add support for live packet capture under windows
    TODO: examine and fix all the warnings

---
+++ support/other/ide/vs2017/Flower/x64/flower.bat
@@ -0,0 +1,3 @@
+del /y ..\..\..\..\..\..\data\flower\output\*.dat
+Debug\flower -s test --config-file="..\..\..\..\..\..\conf\flower.conf" --output-data-dir="..\..\..\..\..\..\data\flower\output" "..\..\..\..\..\..\data\flower\input\4pkts.pcap"
+
